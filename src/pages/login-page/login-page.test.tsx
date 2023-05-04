6f69ced8cf35b58ca5071e34b3410fe273964338
Juan Antonio Seco Merchán <juan.seco@atmira.com>
2023-05-04 14:07:14 +0200

    Add form validation for required fields

--- src/pages/login-page/login-page.test.tsx
+++ src/pages/login-page/login-page.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import { LoginPage } from './login-page'
 
@@ -12,4 +13,17 @@ test('it should renders the form elements', () => {
   render(<LoginPage />)
 
   expect(screen.getByLabelText(/email/i)).toBeInTheDocument()
+  expect(screen.getByLabelText(/password/i)).toBeInTheDocument()
+  expect(screen.getByRole('button', { name: /submit/i })).toBeInTheDocument()
+})
+
+test('it should validate the inputs as required', async () => {
+  render(<LoginPage />)
+
+  userEvent.click(screen.getByRole('button', { name: /submit/i }))
+
+  expect(await screen.findByText(/The email is required/i)).toBeInTheDocument()
+  expect(
+    await screen.findByText(/The password is required/i)
+  ).toBeInTheDocument()
 })
--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -1,7 +1,47 @@
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { useState } from 'react'
+
 export const LoginPage = () => {
+  const [emailErrorMessage, setEmailErrorMessage] = useState<string>('')
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState<string>('')
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    const { email, password } = event.currentTarget
+      .elements as typeof event.currentTarget.elements & {
+      email: HTMLInputElement
+      password: HTMLInputElement
+    }
+
+    if (!email.value) setEmailErrorMessage('The email is required')
+    if (!password.value) setPasswordErrorMessage('The password is required')
+  }
+
   return (
     <div>
       <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="email">Email</Label>
+          <Input type="email" name="email" id="email" placeholder="Email" />
+          {emailErrorMessage && <p>{emailErrorMessage}</p>}
+
+          <Label htmlFor="password">Password</Label>
+          <Input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+          {passwordErrorMessage && <p>{passwordErrorMessage}</p>}
+
+          <button type="submit" name="submit">
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   )
 }
