a4c371f2c1ecaa703b274ce4e540ce99d36f5156
Juan Antonio Seco Merchán <juan.seco@atmira.com>
2023-05-04 16:49:53 +0200

    Refactor form validating by react-hook-form

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -1,41 +1,43 @@
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+type Inputs = {
+  email: string
+  password: string
+}
 
 export const LoginPage = () => {
-  const [emailErrorMessage, setEmailErrorMessage] = useState<string>('')
-  const [passwordErrorMessage, setPasswordErrorMessage] = useState<string>('')
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<Inputs>()
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-
-    const { email, password } = event.currentTarget
-      .elements as typeof event.currentTarget.elements & {
-      email: HTMLInputElement
-      password: HTMLInputElement
-    }
-
-    if (!email.value) setEmailErrorMessage('The email is required')
-    if (!password.value) setPasswordErrorMessage('The password is required')
-  }
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
 
   return (
     <div>
       <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="grid w-full max-w-sm items-center gap-1.5">
           <Label htmlFor="email">Email</Label>
-          <Input type="email" name="email" id="email" placeholder="Email" />
-          {emailErrorMessage && <p>{emailErrorMessage}</p>}
+          <Input
+            type="email"
+            id="email"
+            placeholder="Email"
+            {...register('email', { required: true })}
+          />
+          {errors.email && <p>The email is required</p>}
 
           <Label htmlFor="password">Password</Label>
           <Input
             type="password"
-            name="password"
             id="password"
             placeholder="Password"
+            {...register('password', { required: true })}
           />
-          {passwordErrorMessage && <p>{passwordErrorMessage}</p>}
+          {errors.password && <p>The password is required</p>}
 
           <button type="submit" name="submit">
             Submit
