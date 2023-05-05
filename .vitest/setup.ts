dfac5293a5e9207c5d0897281871c691ae7f0d3a
Juan Antonio Seco Merchán <juan.seco@atmira.com>
2023-05-05 08:58:13 +0200

    Add msw for api testing

--- .vitest/setup.ts
+++ .vitest/setup.ts
@@ -1,4 +1,12 @@
 import matchers from '@testing-library/jest-dom/matchers'
-import { expect } from 'vitest'
+import { afterAll, afterEach, beforeAll, expect } from 'vitest'
+import { server } from '../src/mocks/server'
 
 expect.extend(matchers)
+
+
+beforeAll(() => server.listen())
+
+afterEach(() => server.resetHandlers())
+
+afterAll(() => server.close())
